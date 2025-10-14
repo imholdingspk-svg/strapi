9f7f7fb6e694cfdcb0ab8c7a5687a2b3227cd17c
imholdingspk-svg <imholdingspk@gmail.com>
2025-10-15 00:34:34 +0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,12 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  url: env('URL'),
-  proxy: true
+  url: env('PUBLIC_URL', 'https://admin.salarholdings.pk'),
+  app: { keys: env.array('APP_KEYS') },
+  logger: { updates: { enabled: false } },
 });
